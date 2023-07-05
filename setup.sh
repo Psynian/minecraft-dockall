b274e662330bad481079faaf47039ed3e3ed8c88
Andrew Jacklin <Andrewjacklin@gmail.com>
2023-07-05 12:08:16 +0100

    Directory changes and move arounds

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 wget https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/bungeecord -O bungeecord/plugins/Geyser-BungeeCord.jar
+wget https://download.geysermc.org/v2/projects/floodgate/versions/latest/builds/latest/downloads/bungee -O bungeecord/plugins/floodgate-bungee.jar
+
 
